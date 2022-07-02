0e8e33338eb2a746c4f8eabe94de5b32fd28d96d
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2022-07-02 23:58:15 +0900

    chore:http, ws 를 이용한 server 재설정

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
-import  express  from "express";
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+// http & ws 통합 서버 (http 서버 위에 ws)
+
+function handleConnection(socket){
+    console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
