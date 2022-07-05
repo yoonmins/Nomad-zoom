6c9c586db7d0a061e8ddb82498a83c0b97c4beab
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2022-07-05 23:18:11 +0900

    chore: set nickname and lice chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,14 @@
 // FE 영역
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
 // socket응 이용해 be로 메세지 연결
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to server ✓");
@@ -22,9 +28,19 @@ socket.addEventListener("close", () => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value ="";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
     input.value ="";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
-
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -24,12 +24,20 @@ function onSocketClose (){
 const sockets =[];
 
 wss.on("connection", (socket) =>{
-    sockets.push(socket)
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✓");
     socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
 });
 
 server.listen(3000, handleListen);
+
