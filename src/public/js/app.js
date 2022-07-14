0c1ca55af27875c9b9b944d2b2746b3b73781861
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2022-07-14 16:24:08 +0900

    chore: nwe soket.io server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,15 @@
-// FE 영역
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-const socket = new WebSocket(`ws://${window.location.host}`);
-// socket응 이용해 be로 메세지 연결
+const socket = io();
 
-function makeMessage(type, payload){
-    const msg = {type, payload};
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("open", () => {
-    console.log("Connected to server ✓");
-});
-
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconneted to server ❌");
-});
-
-
-function handleSubmit(event){
+function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    const li = document.createElement("li");
-    li.innerText = `You: ${input.value}`;
-    messageList.append(li);
-    input.value ="";
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+    });
+    input.value = "";
 }
 
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value ="";
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
